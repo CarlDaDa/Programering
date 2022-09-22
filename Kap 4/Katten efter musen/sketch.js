3cd91fd205b149cf41b778fe65a7cbfab5811f7d
CarlDaDa <christian@strandkaer.dk>
2022-09-22 15:43:00 +0200

    improvements and comments

--- Kap 4/Katten efter musen/sketch.js
+++ Kap 4/Katten efter musen/sketch.js
@@ -16,6 +16,7 @@ let eaten;
 let point;
 let catSpeed;
 let level;
+let RESET;
 
 function setup() {
   createCanvas(640, 360);
@@ -31,9 +32,10 @@ function setup() {
   multip = 0.3;
   OST();
   eaten = false;
-  point = 19;
-  catSpeed = 5;
+  point = 0;
+  catSpeed = 0;
   level = 1;
+  RESET = false;
 }
 
 function draw() {
@@ -55,19 +57,40 @@ function draw() {
     OST();
     point += 1;
   }
-
-  if (point < 20) {
+  //finds the distance from player to "cat" and player to "ost" if there's collision it either resets or gives you a point.
+  if (level == 1) {
+    rect(width / 2 - 50, height / 2 - 30, 100, 60);
+    if (keyIsDown(LEFT_ARROW)) {
+      level += 1;
+      catSpeed = 5;
+    }
+  }
+  // level system, the very amputated menu, wich now is just a rect, takes you to "level 2"
+  if (level == 2) {
     background(0, 120, 0);
     fill(255, 255, 0);
     circle(ostX, ostY, d / 2);
     fill(139, 69, 19);
     rect(catx - d / 2, caty - d / 2, d, d, 3);
+    if (point >= 20) {
+      level += 1;
+    }
+
     if (point >= 10) {
       fill(204, 186, 120);
     } else fill(139, 69, 19);
     ellipse(x, y, d + point, d + point);
-    ellipse(x, y, d, d);
-  } else {
+    // ellipse(x, y, d, d);
+  }
+  //changes the color of the player, should be gold, but well... also the player grows every time they eat an "ost"
+  if (level == 3) {
+    if (RESET == false) {
+      catx = random(d, width - d);
+      caty = random(d, height - d);
+      catSpeed = 5;
+      border = 0;
+      RESET = true;
+    }
     background(0, 0, 255);
     fill(255, 255, 0);
     circle(ostX, ostY, d / 2);
@@ -76,6 +99,7 @@ function draw() {
     ellipse(x, y, d + point, d + point);
     speed = 8;
   }
+  // the last levet. resets the cats position and speed. also increases players speed.
 
   strokeWeight(1);
 
@@ -102,12 +126,12 @@ function draw() {
       y += speed;
     }
   }
-
+  //detects if player is in the canvas, and then moves the player.
   x += a;
   y += b;
   catx += catA;
   bordercheck();
-  console.log(catSpeed);
+  console.log(level, point, catSpeed);
 }
 function bordercheck() {
   if (catx + d / 2 >= width) {
@@ -131,7 +155,9 @@ function bordercheck() {
     }
   }
 }
+// bordercheck, takes account to "growth". kinda wobbly
 function OST() {
   ostX = random(d, width - d);
   ostY = random(d, height - d);
 }
+// trying out functions, and making cheese.
